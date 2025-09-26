d917d62f6378aaa819b3261645b1cae1ee18cd01
Benjamin <benjamin.villarroel3@gmail.com>
2025-09-26 13:37:20 -0300

    feat: class Maze creada de manera basica

---
+++ maze.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# n 25, 50
+class Maze:
+    def __init__(self, n):
+        self.n = n
+        matrix = np.full((n,n), " ")
+        pass
+
+    def printMaze():
+        for fila in matrix:
+            print("".join(fila))
+
+
+
+
